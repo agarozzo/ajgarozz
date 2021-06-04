e9dde420b17d7e133a0cb13fcebe61645542d049
agarozzo <85355173+agarozzo@users.noreply.github.com>
2021-06-04 11:26:20 -0400

    Update buildit.sh

--- buildit.sh
+++ buildit.sh
@@ -1,7 +1,5 @@
 set -xe
 
-dockerd &
-sleep 10
 docker version
 
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 552579051427.dkr.ecr.us-east-2.amazonaws.com
