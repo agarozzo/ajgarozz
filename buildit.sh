77a4041618c2c99cad57f5d59ba7d0725dad4122
agarozzo <85355173+agarozzo@users.noreply.github.com>
2021-06-04 11:23:07 -0400

    Update buildit.sh

--- buildit.sh
+++ buildit.sh
@@ -1,5 +1,9 @@
 set -xe
 
+dockerd &
+sleep 10
+docker version
+
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 552579051427.dkr.ecr.us-east-2.amazonaws.com
 docker build -t 552579051427.dkr.ecr.us-east-2.amazonaws.com/ajgarozz:alpine .
 docker push 552579051427.dkr.ecr.us-east-2.amazonaws.com/ajgarozz:alpine
